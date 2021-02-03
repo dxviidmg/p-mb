2e139211b2829d47e9bff92b145f81ec39c53319
David <david@ixulabs.com>
2021-02-02 20:04:43 -0600

    mismos articulos con diferente precio

--- articulos/admin.py
+++ articulos/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from articulos.models import Articulo
+from articulos.models import *
 
 admin.site.register(Articulo)
+admin.site.register(ProveedorArticulo)
--- articulos/models.py
+++ articulos/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from proveedores.models import Proveedor
+from core.models import *
 
-class Articulo(models.Model):
-    proveedor = models.ManyToManyField(Proveedor)
+class Articulo(Tiempo):
     codigo = models.CharField(max_length=30)
     descripcion = models.TextField()
+
+class ProveedorArticulo(Tiempo):
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
     precio = models.DecimalField(max_digits=20, decimal_places=2)
